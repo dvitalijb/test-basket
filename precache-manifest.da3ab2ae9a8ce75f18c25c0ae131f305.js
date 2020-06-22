81e0dd43c349fdc56fa625fe389ffa4aea66cc3f
admin <admin@admins-MacBook-Pro.local>
2020-06-22 15:15:15 +0300

    Updates

---
+++ precache-manifest.da3ab2ae9a8ce75f18c25c0ae131f305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea7e4fd7102739823c6883d45aecc5",
+    "url": "/test-basket/index.html"
+  },
+  {
+    "revision": "bbf81d980d0007ad9ec0",
+    "url": "/test-basket/static/css/main.03fc8b3c.chunk.css"
+  },
+  {
+    "revision": "3b9eb220b01a6982b7e9",
+    "url": "/test-basket/static/js/2.a0969b89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-basket/static/js/2.a0969b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf81d980d0007ad9ec0",
+    "url": "/test-basket/static/js/main.381a6923.chunk.js"
+  },
+  {
+    "revision": "c51e02e751dcb9d4ce9a",
+    "url": "/test-basket/static/js/runtime-main.6d0c95ae.js"
+  }
+]);
